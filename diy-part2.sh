6c8a6d0bb25a493dc84c2b5517465b0c6a4b6a62
Azusachan <31366253+Azusachan@users.noreply.github.com>
2024-06-21 15:51:20 +1000

    remove unneeded argon

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,9 +8,6 @@
 # Blog: https://p3terx.com
 #============================================================
 
-git clone -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
 # add 1.22.x golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
