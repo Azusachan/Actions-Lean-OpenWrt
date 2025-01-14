81d7689039f434b31856f0ba6df584d994b2f699
Azusachan <31366253+Azusachan@users.noreply.github.com>
2025-01-14 22:02:38 +0800

    add argon sources

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,8 @@
 
 # Add a feed source
 #echo "src-git passwallpackages https://github.com/kenzok8/small-package.git" >> "feeds.conf.default"
+echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> "feeds.conf.default"
+echo "src-git argon-config https://github.com/jerrykuku/luci-app-argon-config.git" >> "feeds.conf.default"
 echo "src-git openwrt-passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
 echo "src-git openwrt-passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
 echo "src-git openwrt-passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> "feeds.conf.default"
